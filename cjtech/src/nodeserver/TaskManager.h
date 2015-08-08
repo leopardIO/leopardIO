2a5a5505aa0afbfd0b7448485e115c4b9ff56ed9
apophise <zbzcsn@qq.com>
2015-08-08 07:10:05 +0000

    abc

---
+++ cjtech/src/nodeserver/TaskManager.h
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: TaskManager.h
+	> Author: cooperz
+	> Mail: zbzcsn@qq.com
+	> Created Time: Sat 01 Aug 2015 01:50:15 AM UTC
+ ************************************************************************/
+
+#ifndef _TASKMANAGER_H
+#define _TASKMANAGER_H
+#endif
