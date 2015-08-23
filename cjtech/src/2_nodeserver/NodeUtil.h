5d32724b306a53d8d33710ecfd2daea1766f4604
zhonghongxia <252575160@qq.com>
2015-08-23 08:24:52 +0000

    zhengli

---
+++ cjtech/src/2_nodeserver/NodeUtil.h
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: NodeUtil.h
+	> Author: cooperz
+	> Mail: zbzcsn@qq.com
+	> Created Time: Sat 08 Aug 2015 07:35:19 AM UTC
+ ************************************************************************/
+
+#ifndef _NODEUTIL_H
+#define _NODEUTIL_H
+
+#include "../../common/includeopencv/interface.h"
+#include "DBToMap.h"
+
+#define TMP_PATH "../picsource/tmp/"
+#define TRANDIR "../picsource/traindir/"
+#define FEATUREPATH "../picsource/dest/"
+#define INDEXPATH "../picsource/picindex"
+
+
+#endif
